008f33d6ecd60d8e2564a9e0aca1d7f064ce07e9
Jonas Rishøj <jonas@gefiber.dk>
2023-03-16 14:04:15 +0100

    Little editing

--- src/layout/componets/ProjektTeaser.jsx
+++ src/layout/componets/ProjektTeaser.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 const ProjektTeaser = () => {
   return (
     <div className='ProjektTeaser'>
-      <div className="projekteaserbox"></div>
-      <div className="projekteaserbox"></div>
+      <div className="projekteaserbox">
+        <div className="imgbox"></div>
+        <div className="textbox"></div>
+      </div>
+      <div className="projekteaserbox">
+        <div className="imgbox"></div>
+        <div className="textbox"></div>
+      </div>
       <button className='tilprojektknap'> SE FLERE PROJEKTER</button>
     </div>
   )
