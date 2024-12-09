edb9eb6162ba09981682c10db2a6bf97a6bbb71e
René <reneskjodt333@gmail.com>
2024-12-09 14:09:29 +0100

    Har fjernet vandret scrollbar og gjort footer sidder i bunden af siden hele tiden

--- src/layout/componets/ProjektTeaser.jsx
+++ src/layout/componets/ProjektTeaser.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import ftatravel from '../assets/img/ftatravel.png'
-import typescript from '../assets/img/typescript.png'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import ftatravel from "../assets/img/ftatravel.png";
+import typescript from "../assets/img/TypeScript.png";
 
 const ProjektTeaser = () => {
   return (
-    <div className='ProjektTeaser'>
+    <div className="ProjektTeaser">
       <div className="projekteaserbox">
         <div className="imgbox">
-          <img className="img" src={ftatravel} alt="Forside af FTA-Travel skoleprojekt" />
+          <img
+            className="img"
+            src={ftatravel}
+            alt="Forside af FTA-Travel skoleprojekt"
+          />
         </div>
         <div className="textbox">
           <h3>FTA-Travel</h3>
           <p>
-            FTA-Travel er en prøveeksamens opgave som jeg fik fra skolen i Oktober 2022...
+            FTA-Travel er en prøveeksamens opgave som jeg fik fra skolen i
+            Oktober 2022...
             <br />
             <br />
             <strong>Du kan lære mere om projektet på Projekter siden.</strong>
@@ -23,11 +27,13 @@ const ProjektTeaser = () => {
       </div>
       <div className="projekteaserbox">
         <div className="imgbox">
-        <img className="img" src={typescript} alt="TypeScript skoleprojekt" />
+          <img className="img" src={typescript} alt="TypeScript skoleprojekt" />
         </div>
         <div className="textbox">
           <h3>TypeScript opgave</h3>
-          <p>TypeScript opgaven havde var delt op i 2 måder at lave den på, hvor jeg skulle lave begge, det to mig omkring 2 uger...
+          <p>
+            TypeScript opgaven havde var delt op i 2 måder at lave den på, hvor
+            jeg skulle lave begge, det to mig omkring 2 uger...
             <br />
             <br />
             <strong>Du kan lære mere om projektet på Projekter siden.</strong>
@@ -35,10 +41,10 @@ const ProjektTeaser = () => {
         </div>
       </div>
       <Link to="projekter">
-      <button className='tilprojektknap'> SE FLERE PROJEKTER</button>
+        <button className="tilprojektknap"> SE FLERE PROJEKTER</button>
       </Link>
     </div>
-  )
-}
+  );
+};
 
-export default ProjektTeaser
+export default ProjektTeaser;
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 
-import ftatravel from '../layout/assets/img/ftatravel.png'
-import typescript from '../layout/assets/img/typescript.png'
-import eksamensopgave from '../layout/assets/img/Eksamensopgave.png'
+import ftatravel from "../layout/assets/img/ftatravel.png";
+import typescript from "../layout/assets/img/TypeScript.png";
+import eksamensopgave from "../layout/assets/img/Eksamensopgave.png";
 
 const Projects = () => {
   return (
@@ -15,36 +15,54 @@ const Projects = () => {
       <div className="headline">
         <h1>Mine Projekter</h1>
         <br />
-        <p>Her er nogle eksempler på tidligere projekter jeg har lavet på Viden-Djurs under min uddannelse.</p>
+        <p>
+          Her er nogle eksempler på tidligere projekter jeg har lavet på
+          Viden-Djurs under min uddannelse.
+        </p>
       </div>
 
       <div className="Projekter">
         <div className="ProjekterBox">
           <div className="imgbox">
-            <img className="img" src={ ftatravel } alt="Forside af FTA-Travel skoleprojekt" />
+            <img
+              className="img"
+              src={ftatravel}
+              alt="Forside af FTA-Travel skoleprojekt"
+            />
           </div>
           <div className="textbox">
             <h3>FTA-Travel</h3>
             <p>
-              FTA-Travel er en prøveeksamens opgave som jeg fik fra skolen i Oktober 2022.
+              FTA-Travel er en prøveeksamens opgave som jeg fik fra skolen i
+              Oktober 2022.
               <br />
-              Opgaven gik ud på at vi skulle lave en hjemmeside, hvor det skal forestille Formel 1's egen rejsebureau.
+              Opgaven gik ud på at vi skulle lave en hjemmeside, hvor det skal
+              forestille Formel 1's egen rejsebureau.
               <br />
-              Hvor man så kan bestille rejser i en samlet pakke, istedet for at købe div. fly, hotel og pladsbooking til selve ræset.
+              Hvor man så kan bestille rejser i en samlet pakke, istedet for at
+              købe div. fly, hotel og pladsbooking til selve ræset.
             </p>
           </div>
         </div>
         <div className="ProjekterBox">
           <div className="imgbox">
-            <img className="img" src={ typescript } alt="TypeScript skoleprojekt" />
+            <img
+              className="img"
+              src={typescript}
+              alt="TypeScript skoleprojekt"
+            />
           </div>
           <div className="textbox">
             <h3>TypeScript opgave</h3>
-            <p>TypeScript opgaven havde var delt op i 2 måder at lave den på, hvor jeg skulle lave begge, det to mig omkring 2 uger.
+            <p>
+              TypeScript opgaven havde var delt op i 2 måder at lave den på,
+              hvor jeg skulle lave begge, det to mig omkring 2 uger.
               <br />
               Jeg fulgte et kompendie fra min lærrere.
               <br />
-              Der var 2 forskellige ogaver der omhandlede TypeScript. Den ene var at lave siden udelukne ud af TypeScript, og den anden var at lave en blanding af React JS og TypeScript.
+              Der var 2 forskellige ogaver der omhandlede TypeScript. Den ene
+              var at lave siden udelukne ud af TypeScript, og den anden var at
+              lave en blanding af React JS og TypeScript.
               <br />
               Screenshottet er billedet af hvordan begge så ud efter
             </p>
@@ -52,17 +70,24 @@ const Projects = () => {
         </div>
         <div className="ProjekterBox">
           <div className="imgbox">
-            <img className="img" src={ eksamensopgave } alt="Forside til Webudvikler eksamensopgave" />
+            <img
+              className="img"
+              src={eksamensopgave}
+              alt="Forside til Webudvikler eksamensopgave"
+            />
           </div>
           <div className="textbox">
             <h3>STRØM-opgave</h3>
-            <p>STRØM-opgaven var en eksamensopgave, som vi fik 5 dage til at lave i November 2022.
+            <p>
+              STRØM-opgaven var en eksamensopgave, som vi fik 5 dage til at lave
+              i November 2022.
               <br />
-              Opgaven gik ud på at vi skulle lave en hjemmeside, hvor det skal forestille et elektriker firma's egen hjemmeside.
+              Opgaven gik ud på at vi skulle lave en hjemmeside, hvor det skal
+              forestille et elektriker firma's egen hjemmeside.
               <br />
-              Hvor man kan læse om hvem de er, hvilken service de kan give, en FAQ på ofte stillet spørgsmål, og nyheder fra firmaet.
+              Hvor man kan læse om hvem de er, hvilken service de kan give, en
+              FAQ på ofte stillet spørgsmål, og nyheder fra firmaet.
             </p>
-
           </div>
         </div>
         <div className="ProjekterBox">
